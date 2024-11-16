81b823b6c1b57ba819b4b4ed521e48cbc0303907
PabloVerchere <pablo.verchere@gmail.com>
2024-11-16 19:02:15 +0100

    heat import

---
+++ heat/src/data.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+# Paramètres
+N = 1000 # Nombre de pas de temps
+n1 = 100 # Nombre de pas de temps avant la décomposition
+r = 10 # Rang de la décomposition
+T = 2.0 # Temps final
+dt = T /N # Pas de temps
+
+I = 100 # Nombre de points de l'espace
+E = 1.0 # Longueur de l'espace
+h = E /I # Pas d'espace
+
+K = 1.0 # Coefficient de diffusion
+lambda_ = dt /(h **2)
+
+
+# Fonctions
+def u(x, t):
+    return np.cos(np.pi * x * t)
+
+def u0(x):
+    return u(x, 0)
+
+def f(x, t): # f(x, t) = (du / dt) - (K * d2u / dx2)
+    return -((np.pi * x) * (np.sin(np.pi * x * t))) + (K * ((np.pi * t) **2) * np.cos(np.pi * x * t))
+    
+def alpha(t):
+    return u(0, t)
+
+def beta(t):
+    return u(E, t)
--- heat/src/main.py
+++ heat/src/main.py
@@ -1,101 +1,125 @@
-
+# Importation des bibliothèques
 import numpy as np
 from numpy import linalg as la
 import scipy.sparse as sp
 from scipy.sparse.linalg import spsolve
+import time
+import matplotlib.pyplot as plt
 
-N=1000
-n1=100
-r=5
-I=100
-T=2
-Dt=T/N
-h=1/I
-lambda_=Dt/(h**2)
+import data
 
-def u(x,t):
-    return t*np.sin(0.5*np.pi*x)
-
-def alpha(t):
-    return 0
-
-def beta(t):
-    return t
-
-def f(x,t):
-    return np.sin(0.5*np.pi*x)+t*((0.5*np.pi)**2)*np.sin(0.5*np.pi*x)
-
-U=np.zeros((I+1,N+1))
-
-x=np.zeros(I+1)
-for i in range(I+1):
-   U[i,0]=u(i*h,0)
-   x[i]=i*h
+import sys
+import os
+# Add the parent directory to the system path
+sys.path.append(os.path.abspath('.'))
+import image_compression.src.fct as fct
 
 
-A = sp.lil_matrix((I+1, I+1))
+start_time = time.time()
 
-A[0,0]=1
-A[I,I]=1
-for i in range(1,I):
-    A[i,i]=1+2*lambda_
-    A[i,i+1]=-lambda_
-    A[i,i-1]=-lambda_
+
+# Initialisation de x et U
+x = np.linspace(0, data.E, data.I +1)
+U = np.zeros((data.I +1, data.N +1))
+U[:, 0] = data.u0(x)
+
+
+# Initialisation de la matrice A
+A = sp.lil_matrix((data.I +1, data.I +1))
+
+A[0, 0] = 1
+A[data.I, data.I] = 1
+
+for i in range(1, data.I):
+    A[i, i] = 1 + (2 * data.lambda_)
+    A[i, i +1] = -data.lambda_
+    A[i, i -1] = -data.lambda_
 
 A = A.tocsc()
 
-b=np.zeros(I+1)
+# Initialisation du membre de droite
+b = np.zeros(data.I +1)
+b[1: data.I] = data.dt * data.f(x[1: data.I], 0)
+b[0] = data.alpha(0)
+b[data.I] = data.beta(0)
 
-for i in range(1,I):
-    b[i]=Dt*f(x[i],0)
-b[0]=alpha(0)
-b[I]=beta(0)
+# A @ U[k+1] = U[k] + dt * f(x, k)
+# Résolution du système linéaire jusqu'au temps n1
+for k in range(1, data.n1 +1):
+    b = b + U[:, k -1]
 
-for k in range(1,n1+1):
-    b=b+U[:,k-1]
-    b[0]=alpha((k+1)*Dt)
-    b[I]=beta((k+1)*Dt)
-    U[:,k]=spsolve(A,b)
-    for i in range(1,I):
-        b[i]=Dt*f(x[i],(k+1)*Dt)
+    b[0] = data.alpha((k +1) * data.dt)
+    b[data.I] = data.beta((k +1) * data.dt)
 
-Uapr=U.copy()
+    U[:, k] = spsolve(A, b)
 
-def decompose_matrix(M):
-    '''Decomposes the matrix M using Singular Value Decomposition (SVD).'''
-    U, S, V = np.linalg.svd(M)  # Perform SVD
-    print('U shape:', U.shape)
-    print('S shape:', S.shape)
-    print('V shape:', V.shape)
+    b[1: data.I] = data.dt * data.f(x[1: data.I], (k +1) * data.dt)
+    
 
-  # Adjust matrix shapes to ensure U (m x n), S (n x n), V (n x n):
-    U = U[:, :S.shape[0]]  # Limit U to the number of singular values
-    S = np.diag(S)  # Convert S (a vector) into a diagonal matrix
-    print('Adjusted U shape:', U.shape)
-    print('Adjusted S shape:', S.shape)
-    print('Adjusted V shape:', V.shape)
+Uapr = U.copy()
 
-    return U, S, V
 
-W,D,V=decompose_matrix(U)
-Wr=W.copy()
-#enlever les dernieres colonnes
-for i in range(r,n1+1):
-    Wr[i,i]=0
 
-A2 = sp.csc_matrix(Wr.T @ A @ Wr)
+W, D, V = fct.decompose_matrix(U)
+Wr = W[:, :data.r].copy() # On garde les r premières colonnes de W
 
-#faire 2 b un pour Uapr
-for k in range(n1+1,N+1):
-    b=b+U[:,k-1]
-    b[0]=alpha((k+1)*Dt)
-    b[I]=beta((k+1)*Dt)
-    U[:,k]=spsolve(A,b)
-    b=Wr.T @ b
-    Uapr[:,k]=Wr @ spsolve(A2,b)
-    b = Wr @ b
-    for i in range(1,I):
-        b[i]=Dt*f(x[i],(k+1)*Dt)
+Ar = sp.csc_matrix(Wr.T @ A @ Wr)
+bapr = b.copy()
 
-#3 erreurs entre u(vraie valeur), U et Uapr
-print("erreur :",la.norm(np.abs(Uapr[:,-1]-U[:,-1])))
+
+for k in range(data.n1 +1, data.N +1):
+    b = b + U[:, k -1]
+
+    b[0] = data.alpha((k +1) * data.dt)
+    b[data.I] = data.beta((k +1) * data.dt)
+
+    U[:, k] = spsolve(A, b)
+
+    b[1: data.I] = data.dt * data.f(x[1: data.I], (k +1) * data.dt)
+
+
+
+    bapr = bapr + Uapr[:, k -1]
+
+    bapr[0] = data.alpha((k +1) * data.dt)
+    bapr[data.I] = data.beta((k +1) * data.dt)
+
+    bapr = Wr.T @ bapr
+    Uapr[:, k] = Wr @ spsolve(Ar, bapr)
+    bapr = Wr @ bapr
+    
+    bapr[1: data.I] = data.dt * data.f(x[1: data.I], (k +1) * data.dt)
+
+
+
+# Affichage des erreurs finales
+print("Erreur entre la solution exacte et la solution approchée classique: ", la.norm(data.u(x, data.T) - U[:, -1]))
+print("Erreur entre la solution exacte et la solution approchée par POD: ", la.norm(data.u(x, data.T) - Uapr[:, -1]))
+print("Erreur entre les 2 solutions approchées: ", la.norm(U[:, -1] - Uapr[:, -1]))
+print("Temps d'exécution : %.2f secondes" %(time.time() - start_time))
+print("Rang de la projection:", data.r)
+
+
+# Affichage des erreurs en fonction des itérations
+erreur_classique = []
+erreur_pod = []
+erreur_2 = []
+
+for k in range(data.N +1):
+    erreur_classique.append(la.norm(data.u(x, k * data.dt) - U[:, k]))
+    erreur_pod.append(la.norm(data.u(x, k * data.dt) - Uapr[:, k]))
+    erreur_2.append(la.norm(U[:, k] - Uapr[:, k]))
+
+# Plot des erreurs
+plt.plot(range(data.N +1), erreur_classique, label ="Erreur classique")
+plt.plot(range(data.N +1), erreur_pod, label ="Erreur POD")
+plt.plot(range(data.N +1), erreur_2, label ="Erreur entre les 2 approches")
+
+plt.axvline(x =data.n1, color ='r', linestyle ='--', label ='n1')
+plt.xlabel("Itérations")
+plt.ylabel("Erreur")
+plt.title("Erreurs en fonction des itérations")
+
+plt.legend()
+plt.grid(True)
+plt.show()
